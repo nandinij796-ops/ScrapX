42b5f01e3cd2b792e48fbaf963fd0c10c470805a
nandinij796-ops <nandinij796@gmail.com>
2026-07-08 10:10:48 +0530

    updated contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,18 +1,21 @@
 import "../contact.css";
-
 function Contact() {
+  const handleSubmit = (e) => {
+  e.preventDefault();
+  alert("Thank you! Your message has been received.");
+};
   return (
     <div className="contact-container">
       <h1>Contact Us</h1>
       <p>Have any questions? We'd love to hear from you.</p>
 
       <div className="contact-info">
-        <p><strong>📞 Phone:</strong> +91 98765 43210</p>
+        <p><strong>📞 Phone:</strong> +91 7976650643</p>
         <p><strong>📧 Email:</strong> support@scrapx.com</p>
         <p><strong>📍 Address:</strong> Jaipur, Rajasthan, India</p>
       </div>
 
-      <form className="contact-form">
+      <form className="contact-form" on onSubmit={handleSubmit}>
         <input type="text" placeholder="Full Name" required />
         <input type="email" placeholder="Email" required />
         <input type="text" placeholder="Subject" required />
